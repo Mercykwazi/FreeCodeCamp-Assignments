d61e626efbe11624384bc67dd946c0920ac8d22e
Your Name <mercymncube3@gmail.com>
2017-11-17 15:50:21 +0000

    my function is now working

--- freecode-camp-javaScript-exercise/drope-element.js
+++ freecode-camp-javaScript-exercise/drope-element.js
@@ -1,8 +1,11 @@
 function dropElements(arr, func) {
-    var array =[];
-   for (var i = 0;i<=arr.length; i++) {
-     if(func(arr[i]) !== false){
-     }
-     }
-     
+    var array = [];
+    for (var i = 0; i <= arr.length; i++) {
+        if (func(arr[i]) !== false) {
+            return arr.slice(i, arr.length);
+        }
+    }
+    return array;
+
 }
+console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1; }))
