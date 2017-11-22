4b15d89ca96d8f7d724f0622d3f27c220562891d
Your Name <mercymncube3@gmail.com>
2017-11-22 14:02:14 +0000

    added a for-loop and a parseInt method

--- freecode-camp-javaScript-exercise/binary-agent.js
+++ freecode-camp-javaScript-exercise/binary-agent.js
@@ -2,5 +2,8 @@
 function binaryAgent(str) {
     var splittedString = str.split(' ');
     var string = [];
+    for (i = 0; i < splittedString.length; i++) {
+        string.push(String.fromCharCode(parseInt(splittedString[i], 2)));
+    }
 }
 console.log(binaryAgent(str))
