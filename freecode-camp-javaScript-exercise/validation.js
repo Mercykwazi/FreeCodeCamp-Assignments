ac91cdd5887be4f22a1794ee43b774bb73463aa5
Your Name <mercymncube3@gmail.com>
2017-11-29 15:18:23 +0000

    added regex that test four conditions

--- freecode-camp-javaScript-exercise/validation.js
+++ freecode-camp-javaScript-exercise/validation.js
@@ -1,5 +1,9 @@
 function telephoneCheck(str) {
-     var checker =/^(\d{10})/;
-         return checker.test(str)
-    }
-    console.log(telephoneCheck("5555555555"))
+    var checker = /^(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    return checker.test(str)
+}
+console.log(telephoneCheck("5555555555"))
+console.log(telephoneCheck("555-555-5555"))
+console.log(telephoneCheck("555 555 5555"))
+console.log(telephoneCheck("(555)555-5555"))
+console.log(telephoneCheck("(555) 555-5555"))
