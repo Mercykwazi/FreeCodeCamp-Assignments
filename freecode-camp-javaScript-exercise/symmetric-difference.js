b177cfef9309ca534851f34789885855c64d7d81
Your Name <mercymncube3@gmail.com>
2017-12-06 11:11:32 +0000

    added a forEach method and an if statement

--- freecode-camp-javaScript-exercise/symmetric-difference.js
+++ freecode-camp-javaScript-exercise/symmetric-difference.js
@@ -3,9 +3,22 @@ function sym() {
     for (var i = 0; i < arguments.length; i++) {
         args.push(arguments[i]);
     }
-     function symDiff(arr, array) {
-         
-     }
+    function symDiff(arr, array) {
+        var result = [];
+
+        arr.forEach(function (item) {
+            if (array.indexOf(item) === -1 && result.indexOf(item) === -1) {
+                result.push(item);
+            }
+        });
+
+        array.forEach(function (item) {
+            if (arr.indexOf(item) === -1 && result.indexOf(item) === -1) {
+                result.push(item);
+            }
+        });
+        return result;
+    }
     return args;
 }
 console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]))
