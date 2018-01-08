1272e31d370f105f2a32b882cb1a8d56848eaba8
Your Name <mercymncube3@gmail.com>
2018-01-08 15:25:24 +0000

    modifed my inventory file

--- freecode-camp-javaScript-exercise/inventory-updater.js
+++ freecode-camp-javaScript-exercise/inventory-updater.js
@@ -1,7 +1,20 @@
-function updateInventory(arr1, arr2) {
-    return ar;
+function filtered(array, array2) {
+    return array.filter(e => e !== array2);
 }
-
+function updateInventory(arr1, arr2) {
+    arr2.forEach(function (element) {
+        for (var i= 0; i< arr1.length; i++) {
+            if (element[1] === arr1[i][1]) {
+                element[0] += arr1[i][0];
+                arr1 = filtered(arr1, arr1[i]);
+            }
+        }
+        if (arr1.indexOf(element) === -1) {
+            arr1.push(element);
+        }
+    });
+    return arr1;
+}
 
 var curInv = [
     [21, "Bowling Ball"],
@@ -16,5 +29,5 @@ var newInv = [
     [67, "Bowling Ball"],
     [7, "Toothpaste"]
 ];
-
 console.log(updateInventory(curInv, newInv));
+
