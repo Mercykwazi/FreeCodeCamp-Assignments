2fab0b34338ed68e834c242d209f83f4d33791b1
Your Name <mercymncube3@gmail.com>
2018-01-12 13:13:05 +0000

    modified my files

--- freecode-camp-javaScript-exercise/permutation.js
+++ freecode-camp-javaScript-exercise/permutation.js
@@ -8,9 +8,10 @@ function permAlone(n, str) {
             permAlone(n - 1,temp)
             if (n % 2 === 0) {
                 var splitted = temp.split("");
-                console.log("this is the splitted string",splitted)
+                console.log("this is the splited string",splitted)
                 splitted.splice(temp[i],1,temp[n-1])
-
+                   temp = splitted.join("");
+           
                 console.log("this is the sliced string",splitted);
             } else {
                 var splitted = temp.split("");
@@ -22,4 +23,32 @@ function permAlone(n, str) {
     }
 }
 console.log(permAlone(3,"aba"))
+/*var temp="";
+function generate(n, a) {
+    temp = a;
+    if (n === 1) {
+        console.log(a);
+    } else {
+        // for i := 0; i < n - 1; i += 1 do
+        //     generate(n - 1, A)
+        //     if n is even then
+        // swap(A[i], A[n - 1])
+        for (var i = 0; i <= n-1; i++) {
+            generate(n - 1, temp);
+            if (n % 2 === 0) {
+                let split = temp.split("");
+                split.splice(i, 1, temp[n - 1]);
+                split.splice(n - 1, 1, temp[i]);
+                temp = split.join("");
+            } else {
+                let split = temp.split("");
+                split.splice(0, 1, temp[n - 1]);
+                split.splice(n - 1, 1, temp[0]);
+                temp = split.join("");
+            }
+        }
+        generate(n - 1, temp);
+    }
 
+}
+console.log(generate(4, "ABCD"));*/
