42c6146e353bed7f0d41d2f3155a923eaefc8541
Your Name <mercymncube3@gmail.com>
2018-01-17 14:42:05 +0000

    modified my file

--- freecode-camp-javaScript-exercise/person.js
+++ freecode-camp-javaScript-exercise/person.js
@@ -2,8 +2,29 @@
 var Person = function(firstAndLast) {
     // Complete the method below and implement the others similarly
     this.getFullName = function() {
-      return "";
+      
+      return firstAndLast;
     };
+  this.getFirstName =function(){
+    var first =firstAndLast.split(" ");
+   return first[0];
+  };
+  this.getLastName =function(){
+   var second =firstAndLast.split(" ");
+    return second[1];
+  };
+  this.setFirstName =function(first){
+    firstAndLast = first + " "+ firstAndLast.split(" ")[1];
+    return firstAndLast;
+  };
+  this.setLastName =function(last){
+    firstAndLast =firstAndLast.split(" ")[0] + " " + last;
+    return firstAndLast;
+  };
+  this.setFullName =function (oneName){
+   firstAndLast =oneName; 
+    return firstAndLast;
+  };
     return firstAndLast;
 };
 
