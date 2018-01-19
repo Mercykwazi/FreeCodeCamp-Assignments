5052ea2689d274e3787c48ac5c17fb7ab9fca14f
Your Name <mercymncube3@gmail.com>
2018-01-19 12:38:17 +0000

    my function is now working

--- freecode-camp-javaScript-exercise/pairWise.js
+++ freecode-camp-javaScript-exercise/pairWise.js
@@ -1,5 +1,23 @@
+
 function pairwise(arr, arg) {
-    return arg;
+    var counter = 0;
+    for (var i = 0; i < arr.length; i++) {
+
+        for (var x = i + 1; x < arr.length; x++) {
+
+            if (arr[i] + arr[x] === arg) {
+                counter += i;
+                counter += x;
+                counter[i] = counter[x] = NaN;
+
+            }
+
+        }
+
+    }
+
+    return counter;
+
 }
 
-console.log(pairwise([1, 4, 2, 3, 0, 5], 7))
+console.log(pairwise([1, 3, 2, 4], 4))
