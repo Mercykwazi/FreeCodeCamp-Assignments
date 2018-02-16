092cfc72f5bff3dbee103ed2eb26a7f488e13a19
Your Name <mercymncube3@gmail.com>
2018-02-16 16:26:05 +0000

    added a new file simon game

---
+++ simon-game/js/index.js
@@ -0,0 +1,76 @@
+  var game = {
+            count: 0,
+            color: ["white"],
+            possibleColors: ["red", "blue", "green", "yellow"],
+            gameInProgress: [],
+            player: [],
+            strict: "Off",
+        };
+
+        function addColor() {
+
+            var randomColor = Math.floor(Math.random() * game.possibleColors.length);
+            game.gameInProgress.push(game.possibleColors[randomColor]);
+            console.log("the value of gameInprogress:", game.gameInProgress)
+
+            document.getElementById("countspace").innerHTML = game.gameInProgress.length;
+            var i = 0;
+            var change = setInterval(function () {
+                console.log(game.gameInProgress[i])
+                document.getElementById(game.gameInProgress[i]).style.background = game.gameInProgress[i];
+                setTimeout(function () {
+                    document.getElementById(game.gameInProgress[i - 1]).style.background = game.color[0];
+                }, 500);
+                i++;
+                if (i == game.gameInProgress.length) {
+                    clearInterval(change)
+                }
+            }, 1000);
+
+
+
+        }
+        function strictMode() {
+            if (game.strict === "Off") {
+                game.strict = "On"
+                document.getElementById("strictbtn").innerHTML = "Strict On"
+                document.getElementById("strictbtn").style.background = "green";
+            } else {
+                game.strict = "Off"
+                document.getElementById("strictbtn").innerHTML = "Strict Off"
+                document.getElementById("strictbtn").style.background = "white";
+            }
+
+        }
+
+        function player(color) {
+
+            game.player.push(color);
+
+            if (game.gameInProgress.length === game.player.length) {
+                if (game.gameInProgress.toString() !== game.player.toString()) {
+                    alert("Sorry! wrong moves,you loose");
+                    restart();
+                } else {
+                    addColor();
+                    game.player = [];
+                }
+            }
+        }
+        function reset() {
+            window.location.reload(true);
+        };
+
+        function startGame() {
+            if (game.gameInProgress.length > 0) {
+                alert('sorry, game already in progress');
+            } else {
+                addColor();
+            }
+        }
+        function restart() {
+            game.gameInProgress = [];
+            game.player = [];
+            startGame();
+
+        }
