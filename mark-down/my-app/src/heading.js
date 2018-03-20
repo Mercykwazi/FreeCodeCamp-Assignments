84a35edfbd91bdcba3a1e0f8a1ecbc238633645f
mercy <you@example.com>
2018-03-20 10:15:44 +0200

    added a new project markdown preview

---
+++ mark-down/my-app/src/heading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default class Heading extends React.Component {
+    render() {
+        return (
+            <p>Welcome to my Mark Down viewer</p>
+        )
+    }
+}
