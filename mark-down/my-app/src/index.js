5f6d8ed7a87ff6e370d282d1989637f7b4108d72
mercykwazi <mercymncube3@gmail.com>
2018-03-20 10:15:44 +0200

    added a new project markdown preview

---
+++ mark-down/my-app/src/index.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Heading from'./heading';
+
+
+// export default class Converter extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = { input: "" };
+// }
+// }
+
+const app = document.getElementById("app")
+ReactDOM.render( <Heading/>,app);
