6ee46f5568fda77085b1d375a6668ff9733f495a
mercy <you@example.com>
2018-03-20 13:22:34 +0200

    complited my assignments

--- mark-down/my-app/src/heading.js
+++ mark-down/my-app/src/heading.js
@@ -2,7 +2,7 @@ import React from 'react';
 export default class Heading extends React.Component {
     render() {
         return (
-            <p>Welcome to my Mark Down viewer</p>
+            <h1>Welcome to my Mark Down viewer</h1>
         )
     }
 }
--- mark-down/my-app/src/index.js
+++ mark-down/my-app/src/index.js
@@ -2,15 +2,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Heading from'./heading';
+import Heading from './heading';
+import Input from './input'
+import Markdown from './mark';
 
+export default class Convertor extends React.Component {
+    constructor() {
+        super();
+        this.state = { input: "" };
+    }
+    changeState(iteam) {
+        this.setState({ input: iteam })
+    }
+  
 
-// export default class Converter extends React.Component {
-//     constructor() {
-//         super();
-//         this.state = { input: "" };
-// }
-// }
-
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+                <Heading />
+                <Input changeState={this.changeState.bind(this)} />
+                <Markdown result = {this.state.input}/>
+            </div>
+        )
+    }
+}
 const app = document.getElementById("app")
-ReactDOM.render( <Heading/>,app);
+ReactDOM.render(<Convertor />, app);
+
+
+
