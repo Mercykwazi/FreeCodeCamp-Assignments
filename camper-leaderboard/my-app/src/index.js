aa8cc8d10f116be6ab81be8eb3714dba6c48d3ad
Mercykwazi <mercymncube3@gmail.com>
2018-04-05 14:12:42 +0200

    my table was not filling up the whole page

--- camper-leaderboard/my-app/src/index.js
+++ camper-leaderboard/my-app/src/index.js
@@ -4,14 +4,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Table from './table'
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 class Main extends React.Component {
   render() {
     return (
       <div>
-         <h1> Camper Leaderboard </h1>
-        <Table/>
+          <h1 class="container-fluid" >  Camper Leaderboard</h1>
+            <Table />
       </div>
+      
     );
   }
 }
