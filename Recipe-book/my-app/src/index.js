5b35bd421c03838b510ee9c1012033b6848f9627
Mercykwazi <mercymncube3@gmail.com>
2018-04-09 16:55:26 +0200

    added a local storage that is perfectly working

--- Recipe-book/my-app/src/index.js
+++ Recipe-book/my-app/src/index.js
@@ -4,11 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Input from './input';
 import './index.css';
-
+import List from './list'
 class Main extends React.Component {
     constructor() {
         super()
-        this.state = { Name: "", Ingredients: '' }
+
+        this.state = { fromLS: [], Name: "", Ingredients: '' }
     }
 
     changeName(item) {
@@ -17,17 +18,40 @@ class Main extends React.Component {
     changeIngredients(item) {
         this.setState({ Ingredients: item })
     }
-    // componentDidMount(){
-    //     alert(this.state.Ingredients)
+    // what() {
+    //     localStorage.setItem('OBJ', JSON.stringify(this.state));
+    //     var test= JSON.parse(localStorage.getItem('OBJ'));
+    // this.setState({fromLS:this.state.fromLS.push({Name: test.Name,Ingredients: test.Ingredients})})
+
+
+    //     console.log("fromLS",this.state.fromLS)
+
     // }
+
+    storeRecipe() {
+        var recipe = { Name: this.state.Name, Ingredients: this.state.Ingredients };
+        var list = this.state.fromLS;
+        list.push(recipe);
+        localStorage.setItem('data', JSON.stringify(list));
+        console.log('fromLs', this.state.fromLS)
+        var parsed = JSON.parse(localStorage.getItem('data'));
+        this.setState({ fromLS: parsed });
+    }
+    componentDidMount() {
+        var parsed = JSON.parse(localStorage.getItem('data'));
+        if (parsed) {
+            this.setState({ fromLS: parsed });
+        }
+    }
+
     render() {
         return (
             <div>
-              {this.state.Ingredients}
                 <h1>Recipe Box</h1>
                 <Input changeName={this.changeName.bind(this)} changeIngredients={this.changeIngredients.bind(this)} />
-                {this.state.Name}
-               
+
+                <button onClick={this.storeRecipe.bind(this)} class="btn btn-secondary" > Add Recipe</button>
+                <List list={this.state.fromLS}/>
             </div>
         )
     }
---
+++ Recipe-book/my-app/src/list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class List extends React.Component {
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                {this.props.list.map(element => { return <div><button>{element.Name}</button><br /> <li>{element.Ingredients.split(",").map(e => { return <ul>{e}</ul> })}</li></div> })}
+                {/* {this.props.list.map(element=>{return<li>{element.Ingredients}</li>})} */}
+
+            </div>
+        )
+    }
+}
