bd2147c14236aa97757004aa3e4b48cbec1aa360
Mercykwazi <mercymncube3@gmail.com>
2018-04-10 09:00:30 +0200

    renamed fromLS variable to fromLocalStorage

--- Recipe-book/my-app/src/list.js
+++ Recipe-book/my-app/src/list.js
@@ -5,8 +5,11 @@ export default class List extends React.Component {
         console.log(this.props)
         return (
             <div>
-                {this.props.list.map(element => { return <div><button>{element.Name}</button><br /> <li>{element.Ingredients.split(",").map(e => { return <ul>{e}</ul> })}</li></div> })}
-                {/* {this.props.list.map(element=>{return<li>{element.Ingredients}</li>})} */}
+                {this.props.list.map(element => {
+                    return <div><button>{element.Name}</button><br />
+                        <li>{element.Ingredients.split(",").map(e => { return <ul>{e}</ul> })}</li></div>
+                })}
+
 
             </div>
         )
