67f2d04693f617f904edc75948a079524e586af3
Mercykwazi <mercymncube3@gmail.com>
2018-04-18 09:27:16 +0200

    modified my files

--- Recipe-book/my-app/src/index.js
+++ Recipe-book/my-app/src/index.js
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Input from './input';
@@ -9,7 +7,7 @@ class Main extends React.Component {
     constructor() {
         super()
 
-        this.state = { fromLocalStorage: [], Name: "", Ingredients: '' }
+        this.state = { fromLS: [], Name: "", Ingredients: '' }
     }
 
     changeName(item) {
@@ -19,34 +17,72 @@ class Main extends React.Component {
         this.setState({ Ingredients: item })
     }
 
-    storeRecipe() {
-        var recipe = { Name: this.state.Name, Ingredients: this.state.Ingredients };
-        var list = this.state.fromLocalStorage;
-        list.push(recipe);
-        localStorage.setItem('data', JSON.stringify(list));
-        console.log('fromLs', this.state.fromLocalStorage)
-        var parsed = JSON.parse(localStorage.getItem('data'));
-        this.setState({ fromLocalStorage: parsed });
-    }
-    componentDidMount() {
-        var parsed = JSON.parse(localStorage.getItem('data'));
-        if (parsed) {
-            this.setState({ fromLocalStorage: parsed });
-        }
+    deleteRecipe(name) {
+        this.state.fromLS.forEach(item => {
+            if (item.Name === name) {
+                this.state.fromLS.splice(this.state.fromLS.indexOf(item), this.state.fromLS.indexOf(item) + 1)
+                this.setState({ fromLS: this.state.fromLS })
+
+
+            }
+        })
     }
 
-    render() {
-        return (
-            <div>
-                <h1>Recipe Box</h1>
-                <Input changeName={this.changeName.bind(this)} changeIngredients={this.changeIngredients.bind(this)} />
-
-                <button onClick={this.storeRecipe.bind(this)} class="btn btn-secondary" > Add Recipe</button>
-                <List list={this.state.fromLocalStorage} />
-            </div>
-        )
+    editRecipe(recipe) {
+        var updatedItems = prompt("please edit your Ingredients", recipe);
+        console.log("updated iteams", updatedItems)
+        console.log("fromLs", this.state.fromLS)
+        var ingredients = this.state.fromLS;
+        const ingr = ingredients.find(ingri => ingri.Ingredients === "Ingredients")
+        console.log('ingr', ingr);
+        
+        
     }
+    //function isCherries(fruit) { 
+    //     return fruit.name === 'cherries';
+    // }
+    // console.log("ingridients", ingridients)
+
+
+    // this.state.fromLS.forEach(iteam => {
+    //     if (iteam.item === item) {
+    //         this.state.fromLS.indexOf(iteam);
+    //         this.state.fromLS.splice(this.state.fromLS.indexOf(iteam), this.state.fromLS.indexOf(iteam), updatedItems);
+    //          localStorage.setItem("editedRecipe", updatedItems);
+    // localStorage.getItem('editedRecipe')
+    // this.setState({ fromLS: localStorage.setItem("editedRecipe", updatedItems) })
+
+    //     }
+    // })
+//}
+
+storeRecipe() {
+    var recipe = { Name: this.state.Name, Ingredients: this.state.Ingredients };
+    var list = this.state.fromLS;
+    list.push(recipe);
+    localStorage.setItem('data', JSON.stringify(list));
+    console.log('fromLs', this.state.fromLS)
+    var parsed = JSON.parse(localStorage.getItem('data'));
+    this.setState({ fromLS: parsed });
+}
+componentDidMount() {
+    var parsed = JSON.parse(localStorage.getItem('data'));
+    if (parsed) {
+        this.setState({ fromLS: parsed });
+    }
+}
+
+
+render() {
+    return (
+        <div>
+            <h1>Recipe Box</h1>
+            <Input changeName={this.changeName.bind(this)} changeIngredients={this.changeIngredients.bind(this)} />
+            <button onClick={this.storeRecipe.bind(this)} class="btn btn-secondary" > Add Recipe</button>
+            <List list={this.state.fromLS} deleteButton={this.deleteRecipe.bind(this)} editButton={this.editRecipe.bind(this)} />
+        </div>
+    )
+}
 }
 const app = document.getElementById("root")
 ReactDOM.render(<Main />, app);
-
