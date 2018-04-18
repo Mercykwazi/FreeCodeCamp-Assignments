fcd314ea81144e6a6358386c17b178b745856091
Mercykwazi <mercymncube3@gmail.com>
2018-04-18 09:27:16 +0200

    modified my files

--- Recipe-book/my-app/src/list.js
+++ Recipe-book/my-app/src/list.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 export default class List extends React.Component {
+    eventHandler(name) {
+        this.props.deleteButton(name)
+    }
+    edit(name) {
+        this.props.editButton(name)
+    }
     render() {
         console.log(this.props)
         return (
             <div>
                 {this.props.list.map(element => {
-                    return <div><button>{element.Name}</button><br />
-                        <li>{element.Ingredients.split(",").map(e => { return <ul>{e}</ul> })}</li></div>
+                    return <div>
+                        < button>{element.Name}</button><br />
+                        <li>{element.Ingredients.split(",").map(e => { return <ul>{e}</ul> })}</li>
+                        <button onClick={() => this.eventHandler(element.Name)}>delete {element.Name} </button>
+                        <button onClick={() => this.edit(element.Ingredients)}>edit {element.Name} </button></div>
                 })}
-
-
             </div>
         )
     }
