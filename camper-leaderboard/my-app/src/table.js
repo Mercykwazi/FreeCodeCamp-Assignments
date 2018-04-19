5e844ba9f0cecf5331584b833028a020bacdc0f4
Mercykwazi <mercymncube3@gmail.com>
2018-04-19 16:27:25 +0200

    modifid my files

--- camper-leaderboard/my-app/src/table.js
+++ camper-leaderboard/my-app/src/table.js
@@ -6,36 +6,23 @@ export default class Table extends React.Component {
         super();
         this.state = {
             storage: [],
-            position: [],
+       
+           
         };
     };
-    recent() {
-        Axios.get("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
-            .then(response => {
-                this.setState({ storage: response.data });
-                for (var i = 1; i < response.data.length; i++) {
-                    this.state.position.push(i)
-                 console.log("position",this.state.position)
-                }
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }
 
-    what() {
-        Axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+
+    getUser(currentApi) {
+        Axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/'+currentApi)
             .then(response => {
                 for (var i = 0; i < response.data.length; i++) {
-                    response.data[i]["number"] = i + 1;        
+                    response.data[i]["number"] = i + 1;
                 }
                 this.setState({ storage: response.data });
-                console.log(response.data)
+
             })
-            .catch(error => {
-                console.log(error)
-            });
     }
+    
 
     render() {
         return (
@@ -45,20 +32,22 @@ export default class Table extends React.Component {
                         <th>#</th>
                         <th>Images</th>
                         <th>Camper-Name</th>
-                        <th ><button  onClick={this.recent.bind(this)}>recent </button></th>
-                        <th >< button  onClick={this.recent.bind(this)}>Point-in-30-Days</button></th>
-                     
+                        <th ><button onClick={()=>this.getUser("recent")}>recent </button></th>
+                        <th >< button onClick={()=>this.getUser("alltime")}>Point-in-30-Days</button></th>
+
                     </thead>
+                    
                     {this.state.storage.map(userData =>
+                     
+                    <tbody>
+                        <td>{userData.number}</td>
 
-                        <tbody>
-                            <td>{userData.number}</td>
+                        <td><img alt="" src={userData.img} /> </td>
+                        <td>{userData.username}</td>
+                        <td>{userData.recent }</td>
+                        <td>{userData.alltime}</td>
 
-                            <td><img alt="" src={userData.img} /> </td>
-                            <td>{userData.username}</td>
-                            <td>{userData.recent}</td>
-                            <td>{userData.alltime}</td>
-                        </tbody>)}
+                    </tbody>)}
 
 
 
@@ -66,7 +55,9 @@ export default class Table extends React.Component {
                 </table>
 
             </div>
-            
+
         )
+
     }
+
 }
