2833f4508d59fa33039e923e9a8f754299bd62c0
Mercykwazi <mercymncube3@gmail.com>
2018-05-08 11:04:15 +0200

    added a new function that that contains alive cells

--- game-of-life/index.js
+++ game-of-life/index.js
@@ -1,14 +1,24 @@
+var gridOfDeadCells = [];
+var gridOfLiveCells = [];
 function grid() {
-    var Grid = [];
     for (var x = 0; x <= 10; x++) {
         for (var y = 0; y <= 10; y++) {
-            Grid.push({
+            gridOfDeadCells.push({
                 x: x,
                 y: y,
                 status: "dead"
             })
         }
+        // return gridOfDeadCells
     }
-    return Grid;
 }
-console.log(grid())
+    console.log(grid())
+
+function liveCells(){
+    var aliveCells=[{x:2,y:1,status:"alive"},{x:2,y:2,status:"alive"},{x:2,y:3,status:"alive"}]
+    console.log("aliveCells",aliveCells)
+
+}
+console.log(liveCells())
+
+
