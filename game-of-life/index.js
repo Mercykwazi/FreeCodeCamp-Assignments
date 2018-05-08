ec335de530f742f226f42ac2d3a30cdb6f748156
Mercykwazi <mercymncube3@gmail.com>
2018-05-08 15:49:19 +0200

    added a function that checks the neighbours surronding a cell

--- game-of-life/index.js
+++ game-of-life/index.js
@@ -14,21 +14,32 @@ function grid() {
     return gridOfDeadCells
 }
 
-
+var board = grid();
 function liveCells() {
-    var bord = grid()
     var aliveCells = [{ x: 2, y: 1, status: "alive" }, { x: 2, y: 2, status: "alive" }, { x: 2, y: 3, status: "alive" }]
-    for (var i = 0; i < bord.length; i++) {
+    for (var i = 0; i < board.length; i++) {
         for (var z = 0; z < aliveCells.length; z++) {
-            if (aliveCells[z].x === bord[i].x && aliveCells[z].y === bord[i].y) {
-                bord[i] = aliveCells[z]
+            if (aliveCells[z].x === board[i].x && aliveCells[z].y === board[i].y) {
+                board[i] = aliveCells[z]
 
             }
         }
     }
-    // console.log("aliveCells", aliveCells)
-    return bord;
+
+    return board;
 }
-console.log(liveCells())
+function getNeighbors() {
+    var neighborsPerCell =[];
+    for (var i = 0; i < board.length; i++) {
+        var x = board[i].x;
+        var y = board[i].y;
+        var neighbors = [{ 'x': x - 1, 'y': y }, { 'x': x - 1, "y": y + 1 }, { "x": x, "y": y + 1 }, { "x": x + 1, "y": y + 1 }, { "x": x + 1, "y": y }, { "x": x + 1, "y": y - 1 }, { "x": x, " y": y - 1 }, { "x": x - 1, "y": y - 1 }];
+        neighborsPerCell.push(neighbors);
+
+    }
+    return neighborsPerCell
+   
+}
+console.log(getNeighbors())
 
 
