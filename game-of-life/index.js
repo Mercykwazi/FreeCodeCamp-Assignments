683d99c132050bd07d5d4955f9d1ab47c0547c67
Mercykwazi <mercymncube3@gmail.com>
2018-05-08 12:01:52 +0200

    added a new function that checks the both the dead and the live cells in the grid

--- game-of-life/index.js
+++ game-of-life/index.js
@@ -1,6 +1,6 @@
-var gridOfDeadCells = [];
-var gridOfLiveCells = [];
+
 function grid() {
+    var gridOfDeadCells = [];
     for (var x = 0; x <= 10; x++) {
         for (var y = 0; y <= 10; y++) {
             gridOfDeadCells.push({
@@ -9,15 +9,25 @@ function grid() {
                 status: "dead"
             })
         }
-        // return gridOfDeadCells
+
     }
+    return gridOfDeadCells
 }
-    console.log(grid())
 
-function liveCells(){
-    var aliveCells=[{x:2,y:1,status:"alive"},{x:2,y:2,status:"alive"},{x:2,y:3,status:"alive"}]
-    console.log("aliveCells",aliveCells)
 
+function liveCells() {
+    var bord = grid()
+    var aliveCells = [{ x: 2, y: 1, status: "alive" }, { x: 2, y: 2, status: "alive" }, { x: 2, y: 3, status: "alive" }]
+    for (var i = 0; i < bord.length; i++) {
+        for (var z = 0; z < aliveCells.length; z++) {
+            if (aliveCells[z].x === bord[i].x && aliveCells[z].y === bord[i].y) {
+                bord[i] = aliveCells[z]
+
+            }
+        }
+    }
+    // console.log("aliveCells", aliveCells)
+    return bord;
 }
 console.log(liveCells())
 
