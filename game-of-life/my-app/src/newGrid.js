781d4338718d4e36617fe56bd10e17c87065a5b8
mercy <you@example.com>
2018-05-15 09:29:18 +0200

    deleted a grid.js that only focused on dead cell and added a newGride that focuses on alive cells

--- game-of-life/my-app/src/newGrid.js
+++ game-of-life/my-app/src/newGrid.js
@@ -6,31 +6,70 @@ var alive = [
 
 function getLiveNeighbors(x, y) {
     var counter = 0;
-    
-    var rightNeighbor = alive.find(c => {
-        return c.x === x && c.y === y + 1;
-    });
-    var leftNeighbor = alive.find(c => {
-        return c.x === x && c.y === y - 1;
-    });
-    var topRight = alive.find(c => {
-        return c.x === x - 1 && c.y === y + 1;
-    });
-    var topLeft = alive.find(c => {
-        return c.x === x - 1 && c.y === y - 1;
-    });
-    var bottomLeft = alive.find(c => {
-        return c.x === x + 1 && c.y === y - 1;
-    });
-    var bottomRight = alive.find(c => {
-        return c.x === x + 1 && c.y === y + 1;
-    });
-    var top = alive.find(c => {
-        return c.x === x - 1 && c.y === y;
-    });
-    var bottom = alive.find(c => {
-        return c.x === x + 1 && c.y === y;
-    });
+    var nextGeneration = [];
+    var actualCell = alive.find(item => {
+        return item.x === x && item.y === y;
+    })
+
+    var leftNeighbor = alive.find(cell => {
+        return cell.x === x - 1 && cell.y === y
+    })
+    var bottomLeftNeighbor = alive.find(cell => {
+        return cell.x === x - 1 && cell.y === y + 1
+    })
+    var downBottomNeighbor = alive.find(cell => {
+        return cell.x === x && cell.y === y + 1
+    })
+    var bottomRightNeighbor = alive.find(cell => {
+        return cell.x === x + 1 && cell.y === y + 1
+    })
+    var rightNeighbor = alive.find(cell => {
+        return cell.x === x + 1 && cell.y === y
+    })
+    var upRightNeighbor = alive.find(cell => {
+        return cell.x === x + 1 && cell.y === y - 1
+    })
+    var upNeighbor = alive.find(cell => {
+        return cell.x === x && cell.y === y - 1
+    })
+    var topLeftNeighbor = alive.find(cell => {
+        return cell.x === x - 1 && cell.y === y - 1
+    })
+
+    if (bottomLeftNeighbor !== undefined) {
+        counter += 1;
+    }
+
+    if (downBottomNeighbor !== undefined) {
+        counter += 1;
+    }
+    if (bottomRightNeighbor !== undefined) {
+        counter += 1;
+    }
+    if (upRightNeighbor !== undefined) {
+        counter += 1;
+    }
+    if (upNeighbor !== undefined) {
+        counter += 1;
+    }
+    if (topLeftNeighbor !== undefined) {
+        counter += 1;
+    }
+    if (leftNeighbor !== undefined) {
+        counter += 1;
+    }
+    if (rightNeighbor !== undefined) {
+        counter += 1;
+    }
+    if (actualCell !== undefined && (counter === 2 || counter === 3)) {
+        nextGeneration.push(actualCell);
+        console.log("actual",actualCell)
+    } else if (actualCell === undefined && counter === 3) {
+        nextGeneration.push({ x: x, y: y, status: "alive" });
+        console.log("actual",nextGeneration)
+    }
+    return nextGeneration;
+
 }
 
 function checkNeighbors() {
@@ -50,19 +89,19 @@ function checkNeighbors() {
         var acc = acc;
         var val = val.x;
         return Math.min(acc, val);
-    }, 1000000000000);
+    }, 2000000000000);
 
     var minY = alive.reduce((acc, val) => {
         var acc = acc;
         var val = val.y;
         return Math.min(acc, val);
-    }, 1000000000000);
+    }, 2000000000000);
 
     for (var i = minX - 1; i < maxX + 1; i++) {
         for (var j = minY - 1; j < maxY + 1; j++) {
-            console.log(i,j)
+            console.log(i, j)
             getLiveNeighbors(i, j)
         }
     }
 }
-checkNeighbors();
+checkNeighbors()
