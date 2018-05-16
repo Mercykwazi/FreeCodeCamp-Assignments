fccc40db29abbba2e2560201ece351791d594511
mercy <you@example.com>
2018-05-16 13:44:43 +0200

    added a function that checks the neighbours of each cell

--- game-of-life/my-app/src/grid.js
+++ game-of-life/my-app/src/grid.js
@@ -19,13 +19,23 @@ function liveCells() {
         for (var z = 0; z < aliveCells.length; z++) {
             if (aliveCells[z].x === board[i].x && aliveCells[z].y === board[i].y) {
                 board[i] = aliveCells[z]
-
             }
         }
     }
-
-
     return board;
 }
+function getNeighbors() {
+    var neighborsPerCell = [];
+    for (var i = 0; i < board.length; i++) {
+        var x = board[i].x;
+        var y = board[i].y;
+        var neighbors = [{ 'x': x - 1, 'y': y }, { 'x': x - 1, "y": y + 1 }, { "x": x, "y": y + 1 }, { "x": x + 1, "y": y + 1 }, { "x": x + 1, "y": y }, { "x": x + 1, "y": y - 1 }, { "x": x, " y": y - 1 }, { "x": x - 1, "y": y - 1 }];
+        var nearestNeighbor = { cell: board[i], AllNeighbors: neighbors };
+        neighborsPerCell.push(nearestNeighbor);
+    }
+    return neighborsPerCell;
+}
+
+
 console.log(liveCells())
-console.log(grid())
+//console.log(grid())
