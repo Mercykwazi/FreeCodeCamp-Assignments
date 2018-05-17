08393ad9e989dfcf06917527b1d5b539a820fc7c
mercy <you@example.com>
2018-05-17 12:48:51 +0200

    modified my files

--- game-of-life/my-app/src/index.js
+++ game-of-life/my-app/src/index.js
@@ -3,45 +3,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { getLiveNeighbors, greedOfDeeadCells, board, nextGeneration, setInter } from "./newGrid";
+import { grid, liveCells, getCellAndAliveNeighbors } from "./grid";
 
 class Main extends React.Component {
     constructor() {
         super()
         this.state = {
-            grid: []
+            grid: grid()
         }
-        this.greedOfDeeadCells = this.greedOfDeeadCells.bind(this)
+
     }
 
-    greedOfDeeadCells() {
-        var deadCells = [];
-        for (var x = 0; x < 10; x++) {
-            for (var y = 0; y < 10; y++) {
-                deadCells.push({ "x": x, "y": y, status: "dead" })
-            }
-        }
-        this.setState({ grid: deadCells })
-        return deadCells
-    }
-    changeStatus(){
-        
-    }
-    setAliveCells() {
-        var newGrid = this.greedOfDeeadCells();
-        for (var i = 0; i < newGrid.length; i++) {
-            for (var j = 0; j < nextGeneration.length; j++) {
-                if (newGrid[i].x === nextGeneration[j].x && newGrid[i].y === nextGeneration[j].y) {
-                    newGrid[i] = nextGeneration[j]
-                }
-            }
-        }
-        return this.nextGeneration
-    }
     componentWillMount() {
-        this.setInter
-        this.setAliveCells()
-        console.log("live", nextGeneration)
+
     }
 
 
