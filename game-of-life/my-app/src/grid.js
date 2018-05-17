349a6b4d59cfe6de6260a123a7ea0f792f76d8f6
mercy <you@example.com>
2018-05-17 10:47:18 +0200

    added a function that returns the live neighbours of each cell

--- game-of-life/my-app/src/grid.js
+++ game-of-life/my-app/src/grid.js
@@ -4,16 +4,16 @@ function grid() {
     for (var x = 0; x < 4; x++) {
         for (var y = 0; y < 4; y++) {
             gridOfDeadCells.push({
-                x: x,
-                y: y,
+                'x': x,
+                'y': y,
                 status: "dead"
             })
         }
     }
     return gridOfDeadCells
 }
-var board = grid();
-function liveCells() {
+
+function liveCells(board) {
     var aliveCells = [{ x: 2, y: 1, status: "alive" }, { x: 2, y: 2, status: "alive" }, { x: 2, y: 3, status: "alive" }]
     for (var i = 0; i < board.length; i++) {
         for (var z = 0; z < aliveCells.length; z++) {
@@ -24,18 +24,30 @@ function liveCells() {
     }
     return board;
 }
-function getNeighbors() {
-    var neighborsPerCell = [];
+function getCellAndAliveNeighbors(board) {
+    var results = [];
     for (var i = 0; i < board.length; i++) {
         var x = board[i].x;
         var y = board[i].y;
-        var neighbors = [{ 'x': x - 1, 'y': y }, { 'x': x - 1, "y": y + 1 }, { "x": x, "y": y + 1 }, { "x": x + 1, "y": y + 1 }, { "x": x + 1, "y": y }, { "x": x + 1, "y": y - 1 }, { "x": x, " y": y - 1 }, { "x": x - 1, "y": y - 1 }];
-        var nearestNeighbor = { cell: board[i], AllNeighbors: neighbors };
-        neighborsPerCell.push(nearestNeighbor);
+        var neighbors = [
+            board.find((cell) => cell.x === x - 1 && cell.y === y),
+            board.find((cell) => cell.x === x - 1 && cell.y === y + 1),
+            board.find((cell) => cell.x === x && cell.y === y + 1),
+            board.find((cell) => cell.x === x + 1 && cell.y === y + 1),
+            board.find((cell) => cell.x === x + 1 && cell.y === y),
+            board.find((cell) => cell.x === x + 1 && cell.y === y - 1),
+            board.find((cell) => cell.x === x && cell.y === y - 1),
+            board.find((cell) => cell.x === x - 1 && cell.y === y - 1)].filter(cell => cell !== undefined)
+
+        results.push({ cell: board[i], AllNeighbors: neighbors.filter(cell => cell.status === "alive") });
+
     }
-    return neighborsPerCell;
+    return results;
 }
 
 
-console.log(liveCells())
-//console.log(grid())
+
+var grid = grid();
+var makeAlive = liveCells(grid)
+var getNeighbors = getCellAndAliveNeighbors(makeAlive);
+console.log('getNeighbors', getNeighbors)
