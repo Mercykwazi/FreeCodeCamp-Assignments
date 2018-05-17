22a56cc81a60488b5bffde19cd6fc28b225a3890
mercykwazi <mercymncube3@gmail.com>
2018-05-17 14:36:14 +0200

    modified my game of life

--- game-of-life/my-app/src/grid.js
+++ game-of-life/my-app/src/grid.js
@@ -1,20 +1,6 @@
 
-function grid() {
-    var gridOfDeadCells = [];
-    for (var x = 0; x < 4; x++) {
-        for (var y = 0; y < 4; y++) {
-            gridOfDeadCells.push({
-                'x': x,
-                'y': y,
-                status: "dead"
-            })
-        }
-    }
-    return gridOfDeadCells
-}
 
-function liveCells(board) {
-    var aliveCells = [{ x: 2, y: 1, status: "alive" }, { x: 2, y: 2, status: "alive" }, { x: 2, y: 3, status: "alive" }]
+function liveCells(board,aliveCells) {
     for (var i = 0; i < board.length; i++) {
         for (var z = 0; z < aliveCells.length; z++) {
             if (aliveCells[z].x === board[i].x && aliveCells[z].y === board[i].y) {
@@ -47,11 +33,12 @@ function getCellAndAliveNeighbors(board) {
 
 
 
-var grid = grid();
-var makeAlive = liveCells(grid)
-var getNeighbors = getCellAndAliveNeighbors(makeAlive);
-console.log('getNeighbors', getNeighbors)
+// var grid = grid();
+// var makeAlive = liveCells(grid)
+// var getNeighbors = getCellAndAliveNeighbors(makeAlive);
+// console.log('getNeighbors', getNeighbors)
+// console.log('grid',grid)
 
 module.exports = {
-    grid, liveCells, getCellAndAliveNeighbors
+    liveCells, getCellAndAliveNeighbors
 }
