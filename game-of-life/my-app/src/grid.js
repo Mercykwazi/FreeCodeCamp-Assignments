cb264cbf1cf9959caa7509c18396fdc847a4865c
mercykwazi <mercymncube3@gmail.com>
2018-06-06 16:07:42 +0200

    added an infinity function for my grid

--- game-of-life/my-app/src/grid.js
+++ game-of-life/my-app/src/grid.js
@@ -1,17 +1,4 @@
-function
-    grid() {
-    var gridOfDeadCells = []; newGeneration
-    for (var x = 0; x < 30; x++) {
-        for (var y = 0; y < 30; y++) {
-            gridOfDeadCells.push({
-                'x': x,
-                'y': y,
-                status: "dead"
-            })
-        }
-    }
-    return gridOfDeadCells
-}
+
 var aliveCells = [
     { x: 2, y: 1, status: "alive" }, { x: 2, y: 2, status: "alive" }, { x: 2, y: 3, status: "alive" }
 ]
@@ -27,7 +14,19 @@ function liveCells(board) {
     return board;
 }
 
-
+function makeGrid(aliveCells) {
+    var displayGrid = [];
+    for (let index = 0; index < 20; index++) {
+        for (let secondIndex = 0; secondIndex < 20; secondIndex++) {
+            var cellFound = aliveCells.find(element => {
+                return element.x === index && element.y === secondIndex
+            })
+            cellFound = cellFound ? { ...cellFound, status: "alive" } : { x: index, y: secondIndex, status: "dead" };
+            displayGrid.push(cellFound);
+        }
+    }
+    return displayGrid
+}
 function getCellAliveNeighbourCount(board) {
     var neighborsPerCell = [];
     for (var i = 0; i < board.length; i++) {
@@ -42,19 +41,29 @@ function getCellAliveNeighbourCount(board) {
     }
 }
 
+
 function minMax(arrayObj) {
-    var allXAxis = [];
-    var allYAxis = [];
+    // console.log("first recieved", arrayObj)
+    var xCoordinates = [];
+    var yCoordinates = [];
     arrayObj.forEach(element => {
-        allXAxis.push(element.x);
-        allYAxis.push(element.y);
+        xCoordinates.push(element.x);
+        yCoordinates.push(element.y);
     });
-    var lowestX = allXAxis.sort((a, b) => a - b)[0];
-    var highestX = allXAxis.sort((a, b) => a - b)[allXAxis.length - 1];
-    var lowestY = allYAxis.sort((a, b) => a - b)[0];
-    var highestY = allYAxis.sort((a, b) => a - b)[allYAxis.length - 1];
-    console.log("lowest", lowestX)
-    return { lowestX: lowestX, highestX: highestX, lowestY: lowestY, highestY: highestY }
+    var lowestX = xCoordinates.sort((a, b) => a - b)[0];
+    var highestX = xCoordinates.sort((a, b) => a - b)[xCoordinates.length - 1];
+    var lowestY = yCoordinates.sort((a, b) => a - b)[0];
+    var highestY = yCoordinates.sort((a, b) => a - b)[yCoordinates.length - 1];
+    var toBeCheckedGrid = [];
+    for (var i = lowestX - 2; i < highestX + 2; i++) {
+        for (var j = lowestY - 2; j < highestY + 2; j++) {
+            var cellIsAlive = arrayObj.find(element => element.x === i && element.y === j);
+            cellIsAlive = cellIsAlive ? { ...cellIsAlive, status: "alive" } : cellIsAlive = { x: i, y: j, status: "dead" };
+            toBeCheckedGrid.push(cellIsAlive)
+        }
+    }
+    // console.log("testing", toBeCheckedGrid)
+    return toBeCheckedGrid
 }
 
 
@@ -67,7 +76,7 @@ function newGeneration(board) {
         var newCell = { ...neighbourAliveCellsCount[i].cell }
         for (var j = 0; j < aliveNeigbours.length; j++) {
             var onlyAcess = [aliveNeigbours[j].x, aliveNeigbours[j].y]
-            var finder = board.find((c) => c.x === onlyAcess[0] && c.y === onlyAcess[1])
+            var finder = board.find((c) => c.x === onlyAcess[0] && c.y === onlyAcess[1]);
             if (finder !== undefined && finder.status === "alive") {
                 if (onlyAlive.indexOf(finder) === -1) {
                     onlyAlive.push(finder)
@@ -91,13 +100,16 @@ function newGeneration(board) {
             nextGeneration.push(newCell)
         }
     }
-    return nextGeneration;
+    var onlyLiving = nextGeneration.filter(word => word.status === "alive")
+    return onlyLiving;
 }
-var grid = grid();
-var makeAlive = liveCells(grid)
-var newGenerations = newGeneration(makeAlive)
-minMax(aliveCells)
+// var grid = grid();
+// var makeAlive = liveCells(grid)
+
+// newGeneration(minMax(aliveCells))
+// makeGrid(aliveCells)
+
 
 module.exports = {
-    getCellAliveNeighbourCount, newGeneration,
+    getCellAliveNeighbourCount, newGeneration, minMax, makeGrid
 }
