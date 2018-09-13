ea3084e52f780d60f14c93e06cd147b1592896ea
mercykwazi <mercymncube3@gmail.com>
2018-09-13 15:41:01 +0200

    modied my app

--- roguelike- dungeon-cawler game/my-app/src/information.js
+++ roguelike- dungeon-cawler game/my-app/src/information.js
@@ -16,7 +16,6 @@ function grid() {
 
 function movePlayer(oldUserLocation, userLocation) {
     var board = grid();
-
     for (var i = 0; i < board.length; i++) {
         for (var z = 0; z < stage.stage1.length; z++) {
             if (stage.stage1[z].x === board[i].x && stage.stage1[z].y === board[i].y) {
@@ -26,19 +25,16 @@ function movePlayer(oldUserLocation, userLocation) {
 
     }
 
-
     var theOldLocation = {};
     var theNewLocation = {};
     var findingOldUserLocation = board.find(element => element.x === oldUserLocation.x && element.y === oldUserLocation.y);
     var findingNewUserLocation = board.find(element => element.x === userLocation.x && element.y === userLocation.y);
     var findingAllTheItemsThatAppearInStagesAndNewLocation = stage.stage1.find(element => element.x === findingNewUserLocation.x && element.y === findingNewUserLocation.y);
-
     if (findingAllTheItemsThatAppearInStagesAndNewLocation === undefined) {
         board[board.indexOf(findingOldUserLocation)].occupied = "player";
         board[board.indexOf(findingNewUserLocation)].occupied = "none";
         theOldLocation = board[board.indexOf(findingOldUserLocation)];
         theNewLocation = board[board.indexOf(findingOldUserLocation)];
-
     } else {
         board[board.indexOf(findingOldUserLocation)].occupied = "none";
         board[board.indexOf(findingOldUserLocation)].display = null;
@@ -50,7 +46,6 @@ function movePlayer(oldUserLocation, userLocation) {
 
 }
 
-
 function enemiesAndHealth() {
     var grid = stage.stage1;
     var i = 0;
@@ -60,9 +55,9 @@ function enemiesAndHealth() {
     while (i < 8) {
         var randomValues = { x: Math.floor(Math.random() * 10), y: Math.floor(Math.random() * 10) };
         var position = grid.find(element => element.x === randomValues.x && element.y === randomValues.y);
-        found = i <=3 ? "Enemies" : "Health";
+        found = i <= 3 ? "Enemies" : "Health";
         if (position) {
-            if (i <=3) {
+            if (i <= 3) {
                 position.occupied = found;
                 enemies.push(position)
                 found = undefined
